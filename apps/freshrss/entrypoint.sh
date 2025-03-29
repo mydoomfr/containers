612aaa4e49a841a2b4303a8e210b859f79d215d0
Benjamin Pinchon <dev@mdfr.me>
2025-03-29 17:48:39 +0100

    feat(freshrss): update 1.26.1-alpine
    Signed-off-by: Benjamin Pinchon <dev@mdfr.me>

---
+++ apps/freshrss/entrypoint.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+php -f ./cli/prepare.php >/dev/null
+
+if [ -n "$FRESHRSS_INSTALL" ]; then
+	# shellcheck disable=SC2046
+	php -f ./cli/do-install.php -- \
+		$(echo "$FRESHRSS_INSTALL" | sed -r 's/[\r\n]+/\n/g' | paste -s -)
+	EXITCODE=$?
+
+	if [ $EXITCODE -eq 3 ]; then
+		echo 'ℹ️ FreshRSS already installed; no change performed.'
+	elif [ $EXITCODE -eq 0 ]; then
+		echo '✅ FreshRSS successfully installed.'
+	else
+		echo '❌ FreshRSS error during installation!'
+		exit $EXITCODE
+	fi
+fi
+
+if [ -n "$FRESHRSS_USER" ]; then
+	# shellcheck disable=SC2046
+	php -f ./cli/create-user.php -- \
+		$(echo "$FRESHRSS_USER" | sed -r 's/[\r\n]+/\n/g' | paste -s -)
+	EXITCODE=$?
+
+	if [ $EXITCODE -eq 3 ]; then
+		echo 'ℹ️ FreshRSS user already exists; no change performed.'
+	elif [ $EXITCODE -eq 0 ]; then
+		echo '✅ FreshRSS user successfully created.'
+		./cli/list-users.php | xargs -n1 ./cli/actualize-user.php --user
+	else
+		echo '❌ FreshRSS error during the creation of a user!'
+		exit $EXITCODE
+	fi
+fi
+
+exec "$@"
